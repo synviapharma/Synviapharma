13c2e5048dbafbcf464fa98ea72175e902d28ae2
synviapharma <synviapharma@gmail.com>
2025-02-16 12:09:10 +0530

    intro-hero video upadated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,7 @@
 
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
+import { timesNewRoman } from "@/utility/fonts-utility";
 
 const HeroSection = () => {
   const videoRef = useRef(null);
@@ -11,7 +12,7 @@ const HeroSection = () => {
   useEffect(() => {
     const video = videoRef.current;
     const heading = headingRef.current;
-    
+
     gsap.fromTo(
       heading,
       { scale: 0, opacity: 0 },
@@ -62,28 +63,30 @@ const HeroSection = () => {
     <div className="relative">
       <video
         ref={videoRef}
-        src="./images/capsule.mp4"
+        src="./images/synvia-intro.mp4"
         autoPlay
         muted
         playsInline
         className="w-full h-auto object-cover"
-        style={{ opacity: 1 }} // Ensure initial opacity is set
+        style={{ opacity: 1 }}
       ></video>
 
       <div className="absolute inset-0 flex items-center justify-center px-4">
         <div
           ref={headingRef}
-          className="p-6 md:p-12 rounded-md max-w-full sm:max-w-[90%] md:max-w-[75%] lg:max-w-[50%]"
+          className="p-6 md:p-12 rounded-md max-w-full sm:max-w-[90%] md:max-w-[75%] lg:max-w-[50%] "
         >
           <h2
+            style={timesNewRoman("700")}
             ref={(el) => (headingTextRef.current[0] = el)}
-            className="text-white text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold text-center drop-shadow-lg py-1 sm:py-2"
+            className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center drop-shadow-lg py-1 "
           >
             Connecting the World
           </h2>
           <h2
+            style={timesNewRoman("700")}
             ref={(el) => (headingTextRef.current[1] = el)}
-            className="text-white text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold text-center drop-shadow-lg py-1 sm:py-2"
+            className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center drop-shadow-lg py-1"
           >
             with a Dose of Hope
           </h2>
